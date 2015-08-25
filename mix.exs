1d0102ee4c8b5820e36d34acb61ce6ae06cc957e
Michal Muskala <michal@muskala.eu>
2015-08-25 23:37:26 +0200

    Add ex_doc for generating documentation

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule Mongo.Ecto.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :mongodb_ecto,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.0",
      deps: deps,
-     test_coverage: [tool: ExCoveralls]]
+     test_coverage: [tool: ExCoveralls],
+     description: description,
+     package: package,
+     docs: docs]
   end
 
   def application do
@@ -17,9 +22,31 @@ defmodule Mongo.Ecto.Mixfile do
     [
       {:mongodb, "~> 0.1"},
       {:ecto, "~> 1.0"},
-      {:inch_ex, only: :docs},
       {:dialyze, "~> 0.2.0", only: :dev},
-      {:excoveralls, "~> 0.3.11", only: :test}
+      {:excoveralls, "~> 0.3.11", only: :test},
+      {:inch_ex, only: :docs},
+      {:earmark, "~> 0.1", only: :docs},
+      {:ex_doc, "~> 0.8", only: :docs}
     ]
+  end
+
+  defp description do
+    """
+    MongoDB adapter for Ecto
+    """
+  end
+
+  defp package do
+    [contributors: ["Michał Muskała"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/michalmuskala/mongodb_ecto"},
+     files: ~w(mix.exs README.md lib)]
+  end
+
+  defp docs do
+    [readme: "README.md",
+     source_url: "https://github.com/michalmuskala/mongodb_ecto",
+     source_ref: "v#{@version}",
+     main: "README"]
   end
 end
