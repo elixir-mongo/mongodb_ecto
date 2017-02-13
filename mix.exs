a04093a5f7a9e7abd783bbf6ef2b84908a103f1f
Michal Muskala <michal@muskala.eu>
2017-02-13 08:28:33 +0100

    Remove coverage tools, as they don't compile

--- mix.exs
+++ mix.exs
@@ -23,7 +23,6 @@ defmodule Mongo.Ecto.Mixfile do
       {:mongodb, "~> 0.2.0"},
       {:ecto, "~> 2.0.0"},
       {:dialyze, "~> 0.2.0", only: :dev},
-      {:excoveralls, "~> 0.5", only: :test},
       {:inch_ex, "~> 0.5", only: :docs},
       {:earmark, "~> 1.0", only: :docs},
       {:ex_doc, "~> 0.11", only: :docs}
