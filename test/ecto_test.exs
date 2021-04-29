48e9bccde9c70a4430adaa3fe0f88e45edb5a065
Scott Ames-Messinger <scott@commoncurriculum.com>
2021-04-28 22:07:16 -0400

    Update number of tests pass

--- test/ecto_test.exs
+++ test/ecto_test.exs
@@ -1,7 +1,7 @@
 # 6/33
 # Code.require_file("../deps/ecto/integration_test/cases/assoc.exs", __DIR__)
 
-# 1/30
+# 0/30
 # Code.require_file("../deps/ecto/integration_test/cases/interval.exs", __DIR__)
 
 # 0/28
@@ -16,5 +16,5 @@
 # 28/125
 # Code.require_file("../deps/ecto/integration_test/cases/repo.exs", __DIR__)
 
-# 5/33
+# 6/33
 # Code.require_file("../deps/ecto/integration_test/cases/type.exs", __DIR__)
