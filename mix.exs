7cf54253f230cfa243c900514a469d919620e525
Scott Ames-Messinger <scott@commoncurriculum.com>
2021-08-16 14:16:32 -0400

    typo

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Mongo.Ecto.Mixfile do
 
   defp deps do
     [
-      {:mongodb, github: "elixir-ecto/mongodb", branch: "ecto-3"},
+      {:mongodb, github: "elixir-mongo/mongodb", branch: "ecto-3"},
       {:ecto, "~> 3.6"},
       {:dialyxir, "~> 1.1.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.8", only: :test},
