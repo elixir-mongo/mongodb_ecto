b1001c380c4820c8f89d8603d0722c01f0db603c
Scott Ames-Messinger <scott@commoncurriculum.com>
2023-03-22 21:24:19 -0400

    Fix docs gen

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Mongo.Ecto.Mixfile do
       {:credo, "~> 1.5.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1.0", only: :dev, runtime: false},
       {:ecto, "~> 3.6"},
-      {:ex_doc, ">= 0.0.0", only: :docs, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.16", only: :test},
       {:mongodb, "~> 1.0.0"},
       {:telemetry, ">= 0.4.0"}
